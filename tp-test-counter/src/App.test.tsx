0f584cce0e79efd0b145f17031e22a19c7f2b733
Frédéric GAURAT <fgaurat@gmail.com>
2023-05-26 14:33:36 +0200

    add unit test

---
+++ tp-test-counter/src/App.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from '@testing-library/user-event'
+
+import App from "./App"
+
+
+
+test('if Vite + React in App',async ()=>{
+    render(<App/>)
+    expect(screen.getByText('Vite + React')).toBeInTheDocument()
+})
+
+test('if initial value==0 in App',async ()=>{
+    render(<App/>)
+    expect(screen.getByText('count is 0')).toBeInTheDocument()
+})
+
+test('increment value in App',async ()=>{
+    render(<App/>)
+    await userEvent.click(screen.getByText('count is 0'))
+    expect(screen.getByText('count is 1')).toBeInTheDocument()
+})
